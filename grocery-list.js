db57247801803ed12c3828cfdef97dc93759b084
jasonbdaniels <jasonbdaniels@gmail.com>
2017-01-29 15:25:26 -0500

    Adds ingredient parseing code [WIP].

--- grocery-list.js
+++ grocery-list.js
@@ -24,5 +24,40 @@ var groceryList = {
     delete allItems[key];
 
     sessionStorage.setItem(this.groceryList, JSON.stringify(allItems));
+  },
+  ingredient: function(key, index) {
+    var measurementRegex = /^\S+\s\S+/;
+  	var amountRegex = /^(\d+\/\d+|\d+\.\d+|\d\-\d|\d+)/;
+    var unitRegex = /\S+$/;
+    var ingredientLine = this.items()[key][index];
+    var ingredientParts = ingredientLine.split(" ");
+
+    if(ingredientParts.length >= 3){
+      var measurement = measurementRegex.exec(ingredientLine);
+      var amount = ingredientParts[0];
+      var unit = ingredientParts[1];
+      var type = ingredientLine.split(amount + " " + unit)[1];
+
+      return {amount: amount, unit: unit, type: type};
+    }
+
+    return {amount: null, unit: null, type: ingredientLine};
+  },
+  measurements: {
+    pound: {
+      varients: ["pound", "pounds", "lb", "lbs"]
+    },
+    ounce: {
+      varients: ["ounce", "ounces", "oz"]
+    },
+    tablespoon: {
+      varients: ["tablespoon", "tablespoons", "tbsp", "tbsps"]
+    },
+    teaspoon: {
+      varients: ["teaspoon", "teaspoons", "tsp", "tsps", "tspn", "tspns"]
+    },
+    cup: {
+      varients: ["cup", "cups"]
+    }
   }
 }
