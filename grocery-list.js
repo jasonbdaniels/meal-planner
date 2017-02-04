4d4f359adf280deb06f6bdddd4436162a79efc74
jasonbdaniels <jasonbdaniels@gmail.com>
2017-02-03 20:53:40 -0500

    Creates measurement ingredient parser.

--- grocery-list.js
+++ grocery-list.js
@@ -27,37 +27,74 @@ var groceryList = {
   },
   ingredient: function(key, index) {
     var measurementRegex = /^\S+\s\S+/;
-  	var amountRegex = /^(\d+\/\d+|\d+\.\d+|\d\-\d|\d+)/;
+  	var amountRegex = /^(\d+\/\d+|\d+\.\d+|\d+\-\d+|\d+)/;
     var unitRegex = /\S+$/;
     var ingredientLine = this.items()[key][index];
     var ingredientParts = ingredientLine.split(" ");
 
     if(ingredientParts.length >= 3){
       var measurement = measurementRegex.exec(ingredientLine);
-      var amount = ingredientParts[0];
-      var unit = ingredientParts[1];
-      var type = ingredientLine.split(amount + " " + unit)[1];
+      var amount = amountRegex.exec(ingredientParts[0]);
+      var unit = this.measurements.unitFrom(ingredientParts[1]);
 
-      return {amount: amount, unit: unit, type: type};
+      if(amount == null){
+        return {amount: null, unit: null, type: ingredientLine};
+      }
+      else if(unit == null){
+        var type = ingredientLine.split(ingredientParts[1])[1];
+
+        return {amount: amount[0], unit: null, type: type};
+      }
+      else {
+          var type = ingredientLine.split(ingredientParts[0] + " " + ingredientParts[1])[1];
+
+          return {amount: amount[0], unit: unit.symbol, type: type};
+      }
     }
 
     return {amount: null, unit: null, type: ingredientLine};
   },
   measurements: {
     pound: {
+      symbol: "lb",
       varients: ["pound", "pounds", "lb", "lbs"]
     },
     ounce: {
+      symbol: "oz",
       varients: ["ounce", "ounces", "oz"]
     },
     tablespoon: {
+      symbol: "tbsp",
       varients: ["tablespoon", "tablespoons", "tbsp", "tbsps"]
     },
     teaspoon: {
+      symbol: "tspn",
       varients: ["teaspoon", "teaspoons", "tsp", "tsps", "tspn", "tspns"]
     },
     cup: {
+      symbol: "cup",
       varients: ["cup", "cups"]
+    },
+    unitFrom: function(unit) {
+      unit = unit.toLowerCase();
+
+      if(this.pound.varients.indexOf(unit) > -1) {
+        return this.pound;
+      }
+      else if(this.ounce.varients.indexOf(unit) > -1) {
+        return this.ounce;
+      }
+      else if(this.tablespoon.varients.indexOf(unit) > -1) {
+        return this.tablespoon;
+      }
+      else if(this.teaspoon.varients.indexOf(unit) > -1) {
+        return this.teaspoon;
+      }
+      else if(this.cup.varients.indexOf(unit) > -1) {
+        return this.cup;
+      }
+
+      return null;
     }
   }
 }
