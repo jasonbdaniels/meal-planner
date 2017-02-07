f459325416f2c65555d2d4a9e8eb4ede32763169
jasonbdaniels <jasonbdaniels@gmail.com>
2017-02-06 21:36:55 -0500

    Creates add function for measurements.

--- grocery-list.js
+++ grocery-list.js
@@ -36,42 +36,71 @@ var groceryList = {
       var measurement = measurementRegex.exec(ingredientLine);
       var amount = amountRegex.exec(ingredientParts[0]);
       var unit = this.measurements.unitFrom(ingredientParts[1]);
+      var unitSymbol;
+      var ingredientType;
 
       if(amount == null){
-        return {amount: null, unit: null, type: ingredientLine};
+        return {amount: null, unit: null, type: this.normalizeIngredient(ingredientLine)};
       }
       else if(unit == null){
-        var type = ingredientLine.split(ingredientParts[1])[1];
+        ingredientParts.shift();
 
-        return {amount: amount[0], unit: null, type: type};
+        var type = ingredientParts.join(" ");
+
+        return {amount: amount[0], unit: null, type: this.normalizeIngredient(type)};
       }
       else {
-          var type = ingredientLine.split(ingredientParts[0] + " " + ingredientParts[1])[1];
+        ingredientParts.shift();
+        ingredientParts.shift();
 
-          return {amount: amount[0], unit: unit.symbol, type: type};
+        var type = ingredientParts.join(" ");
+
+        return {amount: amount[0], unit: unit.symbol, type: this.normalizeIngredient(type)};
       }
     }
 
-    return {amount: null, unit: null, type: ingredientLine};
+    return {amount: null, unit: null, type: this.normalizeIngredient(ingredientLine)};
+  },
+  normalizeIngredient: function(ingredient){
+    var parenthesesRegex = /\(.+\)/g;
+    var commaSeperationRegex = /,.+$/g;
+    var articleRegex = /\s(a|the|an)\s/g;
+    var prepositionRegex = /\s(from|of|by|on|in|off|out|at|about)\s/g;
+
+    ingredient = ingredient.replace(commaSeperationRegex, "");
+    ingredient = ingredient.replace(parenthesesRegex, "");
+    ingredient = ingredient.replace(articleRegex, " ");
+    ingredient = ingredient.replace(prepositionRegex, " ");
+
+    return ingredient;
   },
   measurements: {
     pound: {
+      deminsion: "weight",
       symbol: "lb",
       varients: ["pound", "pounds", "lb", "lbs"]
     },
     ounce: {
+      deminsion: "volume",
+      factor: 1,
       symbol: "oz",
       varients: ["ounce", "ounces", "oz"]
     },
     tablespoon: {
+      deminsion: "volume",
+      factor: 0.5,
       symbol: "tbsp",
       varients: ["tablespoon", "tablespoons", "tbsp", "tbsps"]
     },
     teaspoon: {
+      deminsion: "volume",
+      factor: 0.1667,
       symbol: "tspn",
       varients: ["teaspoon", "teaspoons", "tsp", "tsps", "tspn", "tspns"]
     },
     cup: {
+      deminsion: "volume",
+      factor: 8,
       symbol: "cup",
       varients: ["cup", "cups"]
     },
@@ -92,6 +121,17 @@ var groceryList = {
       }
       else if(this.cup.varients.indexOf(unit) > -1) {
         return this.cup;
+      }
+
+      return null;
+    }
+    add: function(lAmount, lMeasurement, rAmount, rMeasurement) {
+      if(lMeasurement.deminsion == rMeasurement.deminsion){
+        var lhs = lAmount*lMeasurement.factor;
+        var rhs = rAmount*rMeasurement.factor;
+        var amount = lhs + rhs;
+
+        return {amount: amount, measurement: this.ounce};
       }
 
       return null;
